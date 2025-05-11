1413f5b21b840cdea4d64acd869423167d7da042
laurence702 <akaigbokwelaurence@gmail.com>
2025-05-11 13:23:47 +0100

    add render yaml

--- start.sh
+++ start.sh
@@ -8,9 +8,3 @@ php-fpm -D
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
-
-composer install --no-interaction --no-dev --optimize-autoloader
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan migrate --force --seed 
