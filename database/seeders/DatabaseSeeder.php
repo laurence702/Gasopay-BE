8ee42379d8f0c8a763e57a9f26cd61a9648ac051
laurence702 <akaigbokwelaurence@gmail.com>
2025-06-10 11:28:08 +0100

    use ngn names in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,31 @@ class DatabaseSeeder extends Seeder
 {
     protected $usedPhones = [];
 
+    // Add Nigerian names and places (with emphasis on South-South)
+    protected $nigerianNames = [
+        'Ejiro Ovie', 'Osas Igbinosa', 'Timi Ebi', 'Blessing Ehizogie', 'Chinedu Obus',
+        'Aghogho Ufuoma', 'Onome Pere', 'Doubra Kuro', 'Tariq Bello', 'Godwin Eguavoen',
+        'Kessiana Akpojotor', 'Amen Imuetiyan', 'Ese Uruemu', 'Boma George', 'Tamuno Iyalla',
+        'Ufuoma Precious', 'Oghenekaro Ochuko', 'Eghosa Amen', 'Idowu Funke', 'Adekunle Taiwo'
+    ];
+
+    protected $nigerianAddresses = [
+        '12 Sapele Road, Benin City, Edo State',
+        '5 Effurun-Sapele Road, Warri, Delta State',
+        '8 Azikoro Road, Yenagoa, Bayelsa State',
+        '3 Airport Road, Effurun, Delta State',
+        '10 Ugbowo Road, Benin City, Edo State',
+        '7 Isaac Boro Expressway, Yenagoa, Bayelsa State',
+        '2 Nnebisi Road, Asaba, Delta State',
+        '15 Ekenwan Road, Benin City, Edo State',
+        '9 PTI Road, Effurun, Delta State',
+        '4 Melford Okilo Road, Yenagoa, Bayelsa State'
+    ];
+
+    protected $nigerianPhonePrefixes = [
+        '070', '080', '081', '090', '091'
+    ];
+
     public function run(): void
     {
         //Create branches first
@@ -24,7 +49,7 @@ class DatabaseSeeder extends Seeder
         // Create super admin with a unique phone number
         $superAdminPhone = $this->generateUniquePhone();
         User::factory()->create([
-            'fullname' => 'Super Admin',
+            'fullname' => $this->getRandomName(),
             'email' => 'admin@example.com',
             'phone' => $superAdminPhone,
             'password' => Hash::make('password'),
@@ -37,7 +62,7 @@ class DatabaseSeeder extends Seeder
         $branches->each(function ($branch) {
             $adminPhone = $this->generateUniquePhone();
             User::factory()->create([
-                'fullname' => $branch->name . ' Admin',
+                'fullname' => $this->getRandomName(),
                 'email' => strtolower(str_replace(' ', '', $branch->name)) . '@example.com',
                 'phone' => $adminPhone,
                 'password' => Hash::make('password'),
@@ -66,7 +91,7 @@ class DatabaseSeeder extends Seeder
             $riderPhone = $this->generateUniquePhone();
 
             $rider = User::factory()->create([
-                'fullname' => 'Rider User ' . $i,
+                'fullname' => $this->getRandomName(),
                 'email' => 'rider' . $i . '@example.com',
                 'phone' => $riderPhone,
                 'password' => Hash::make('password'),
@@ -81,19 +106,32 @@ class DatabaseSeeder extends Seeder
             UserProfile::factory()->create([
                 'user_id' => $rider->id,
                 'vehicle_type' => Arr::random($vehicleTypes),
-                "guarantors_name" => 'guarantor'. $i,
+                "guarantors_name" => $this->getRandomName(),
                 "guarantors_phone" => $this->generateUniquePhone(),
-                "guarantors_address" => 'guarantor address'. $i,
-                "address" => 'address'. $i,
+                "guarantors_address" => $this->getRandomAddress(),
+                "address" => $this->getRandomAddress(),
                 "profile_pic_url" => '/path/to/image'. $i,
             ]);
         }
        }
 
+    private function getRandomName(): string
+    {
+        return Arr::random($this->nigerianNames);
+    }
+
+    private function getRandomAddress(): string
+    {
+        return Arr::random($this->nigerianAddresses);
+    }
+
     private function generateUniquePhone(): string
     {
         do {
-            $phone = '555' . random_int(1000000, 9999999);
+            // Generate a Nigerian-style phone number
+            $prefix = Arr::random($this->nigerianPhonePrefixes);
+            $suffix = str_pad(random_int(0, 9999999), 7, '0', STR_PAD_LEFT);
+            $phone = $prefix . $suffix;
         } while ($this->phoneExists($phone));
 
         $this->usedPhones[] = $phone;
